6faea2874e11163850fff85194e079dd86fcfc73
lucas234322 <135736120+lucas234322@users.noreply.github.com>
2024-11-06 11:37:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('adventure').addEventListener('click', function() {
+    alert('Você escolheu Aventura! Vamos explorar novos mundos!');
+});
+
+document.getElementById('challenge').addEventListener('click', function() {
+    alert('Você escolheu Desafio! Prepare-se para testar suas habilidades.');
+});
+
+document.getElementById('exit').addEventListener('click', function() {
+    alert('Você escolheu Sair. Até a próxima!');
+});
